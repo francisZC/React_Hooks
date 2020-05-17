48b575e0b97e2deaf3a12c47045f842344efb291
zhengchao <zhengchaochina@gmail.com>
2020-05-17 15:41:38 +0800

    Hooks-state

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//封装一个评论项组件，此组件由于不需要自己的私有数据，所以直接定义为无状态组件
+import Counter from 'redux_test.js'
+
+ReactDOM.render(<Counter>
+
+
+
+</Counter>, document.getElementById('app')
+    )
